745c95c5e011d1399b7e86c4a21ca039b47b9b22
Andrey Gorevoy <andrei.haravy@helmes.com>
2022-09-16 16:48:25 +0200

    Full app ready commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,28 @@
+import csstype.Position
+import csstype.px
+import emotion.react.css
 import kotlinx.browser.document
+import kotlinx.serialization.Serializable
+import react.Fragment
+import react.create
+import react.createElement
+import react.dom.client.createRoot
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.h3
+import react.dom.html.ReactHTML.img
+import react.dom.html.ReactHTML.p
+import react.useState
 
 fun main() {
-    document.bgColor = "red"
+    val container = document.getElementById("root") ?: error("Couldn't find root container!")
+    createRoot(container).render(App.create())
 }
+
+@Serializable
+data class Video(
+    val id: Int,
+    val title: String,
+    val speaker: String,
+    val videoUrl: String
+)
